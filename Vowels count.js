fefde2c4883b07dad75bff5f3264779200087830
nick <108027865+nickaldwin@users.noreply.github.com>
2023-04-26 14:55:13 +0800

    add files via upload because there is no cli

---
+++ Vowels count.js
@@ -0,0 +1,10 @@
+﻿function countVowels(str) {
+  const vowels = "aeiou";
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
