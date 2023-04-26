5ebb9581dd745450b7eb7de60e35bf0f54eeb0c4
nick <108027865+nickaldwin@users.noreply.github.com>
2023-04-26 15:08:08 +0800

    Update Vowels count.js

--- Vowels count.js
+++ Vowels count.js
@@ -1,4 +1,10 @@
-﻿function countVowels(str) {
+/*Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.*/
+
+function countVowels(str) {
   const vowels = "aeiou";
   let count = 0;
   for (let i = 0; i < str.length; i++) {
