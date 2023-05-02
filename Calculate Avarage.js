e59b2b604e4b8db0f319b3920fc943fe3fe1017d
nickaldwin <nicklemoncito98@gmail.com>
2023-05-02 06:09:46 +0000

    adding new files

---
+++ Calculate Avarage.js
@@ -0,0 +1,12 @@
+/*Description:
+Write a function which calculates the average of the numbers in a given list.
+Note: Empty arrays should return 0.*/
+
+//the solution 
+function find_average(arr) {
+    return arr.length > 0? arr.reduce((a, b) => a + b) / arr.length : 0;
+}
+
+
+
+
