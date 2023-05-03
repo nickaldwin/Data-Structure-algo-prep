5999e21ecd25d4e220f42a0000bb51d2cfe48ef9
nick <108027865+nickaldwin@users.noreply.github.com>
2023-05-03 10:59:43 +0800

    Update Highest and Lowest.js

--- Highest and Lowest.js
+++ Highest and Lowest.js
@@ -7,3 +7,8 @@ highAndLow("1 2 -3 4 5"); // return "5 -3"
 highAndLow("1 9 3 4 -5"); // return "9 -5"
 */
 
+//the solution
+function highAndLow(numbers){
+  numbers = numbers.split(' ');
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
