f560deff0ab0b48b59a24c9d7f3371c55174215e
nick <108027865+nickaldwin@users.noreply.github.com>
2023-05-17 15:12:11 +0800

    Create Palandrome.js
    ill come back later

---
+++ Palandrome.js
@@ -0,0 +1,16 @@
+/*function word say is a palanrome*/
+
+
+// PESUDO CODE ACTIVATEd
+//word === str, no empty no ! , no nums, no funny biz no caps. one word
+
+//RETURN TRUE || FALSE 
+// bob => true
+// tacos => false
+//racecar => true
+
+function isPalandrome(word){
+}
+console.log(isPalandrome('racecar'), 'true') // true
+console.log(isPalandrome('bob'), 'true') // true
+console.log(isPalandrome('tacos'), 'false') // true
