26f78a4913f7eb86cb957c734c708c94de81eaf1
nick <108027865+nickaldwin@users.noreply.github.com>
2023-05-24 13:25:19 +0800

    Create Impement sectionSort.js

---
+++ Data-Structure-&-algo-problems/Impement sectionSort.js
@@ -0,0 +1,25 @@
+//the question
+/*
+Implement Selection Sort
+
+Here we will implement selection sort. Selection sort works by selecting the minimum value in a list and swapping it with the first value in the list. It then starts at the second position, selects the smallest value in the remaining list, and swaps it with the second element. It continues iterating through the list and swapping elements until it reaches the end of the list. Now the list is sorted. Selection sort has quadratic time complexity in all cases.
+
+Instructions: Write a function selectionSort which takes an array of integers as input and returns an array of these integers in sorted order from least to greatest.
+
+*/
+//the solution
+function swap(a, b, arr) {
+  let tmp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = tmp;
+}
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[min] > array[j]) min = j;
+    }
+    swap(i, min, array);
+  }
+  return array;
+}
