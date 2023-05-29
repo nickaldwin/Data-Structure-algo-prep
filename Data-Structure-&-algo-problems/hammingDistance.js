eea162f341508359f0f9ddc2a626cd6b27a2c17c
nick lemoncito <nicklemoncito98@gmail.com>
2023-05-29 04:40:01 +0000

    adding files

---
+++ Data-Structure-&-algo-problems/hammingDistance.js
@@ -0,0 +1,23 @@
+/* CHALLENGE
+Given two strings of equal length, calculate and return the the hamming distance.
+E.g hammingDistance('rover', 'river') // should return 1
+*/
+
+//the soluton
+
+function hammingDistance(stringA, stringB){
+    let result = 0
+
+    if (stringA === stringB.length) {
+            for(let i= 0; i<stringA.length; i++){
+                if(stringA[i].toLowerCase() != stringB[i].toLowerCase()){
+                     result++
+                }
+       }
+       return result
+    }else{
+        throw new Error('Strings do not have equal length')
+    }
+}
+
+module.exports = hammingDistance
