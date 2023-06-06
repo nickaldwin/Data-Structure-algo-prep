482455a43b8732a0293f7a7bfe0dee08ac590b63
nick <108027865+nickaldwin@users.noreply.github.com>
2023-06-06 15:30:11 +0800

    Update fizzBuzz.js

--- Data-Structure-&-algo-problems/fizzBuzz.js
+++ Data-Structure-&-algo-problems/fizzBuzz.js
@@ -5,6 +5,23 @@
     multiples of both three and five print “FizzBuzz”.
 */
 
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(100);
+
+
 
 
 function fizzBuzz(n) {
